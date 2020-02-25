b2481b10f9223dca143125eeb56980d63af5be96
joniburn <joniburn@gmail.com>
2020-02-25 16:47:36 +0900

    ボールのバウンスを調整

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider _collider)
     {
         Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Rigidbody rigidBody = GetComponent<Rigidbody>();
+        rigidBody.AddForce(collision.impulse, ForceMode.Impulse);
     }
 
 }
