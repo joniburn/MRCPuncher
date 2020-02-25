d88af3f150f7593f1840ed05fb3cb9f3d40af514
joniburn <joniburn@gmail.com>
2020-02-25 16:38:32 +0900

    ボールをパンチする部分を作成中、バウンス調整中

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider _collider)
+    {
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject m_prefab;
+
+    public float m_intervalSec = 5;
+
+    void OnEnable()
+    {
+        InvokeRepeating("Spawn", m_intervalSec, m_intervalSec);
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void Spawn() {
+        Instantiate(m_prefab, transform);
+    }
+
+}
